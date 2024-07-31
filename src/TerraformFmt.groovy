c52ba8642a5b47485d8357ce4aa9c4ef59cdc36a
Sanchit2323 <153765874+Sanchit2323@users.noreply.github.com>
2024-07-31 19:26:22 +0530

    Update TerraformFmt.groovy

--- src/TerraformFmt.groovy
+++ src/TerraformFmt.groovy
@@ -1,11 +1,9 @@
 package src
 
 class TerraformFmt {
-    static void format() {
-        stage('Terraform Format') {
-            catchError {
-                sh 'terraform fmt -check -diff'
-            }
-        }
+    static void format(String message) {
+        echo "Formatting Terraform: ${message}"
+        sh 'terraform fmt -check' // Example command to check formatting
     }
 }
+
